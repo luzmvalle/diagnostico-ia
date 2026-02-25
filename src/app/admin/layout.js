7be74ee94c3908560bdea5be20d80e3bc1f2a9b3
luzmvalle <marialuzmotavalle21@gmail.com>
2026-02-25 12:42:59 -0300

    Create layout.js

---
+++ src/app/admin/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Admin â DiagnÃ³stico IA',
+  robots: 'noindex, nofollow',
+};
+
+export default function AdminLayout({ children }) {
+  return <>{children}</>;
+}
